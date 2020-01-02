6a3606585ee62784a4e87de493bcfaab05fe8b66
nnakhfor <nnakhfor@deakin.edu.au>
2020-01-02 14:52:03 +1100

    test commit 1

--- app/src/main/java/com/stevecrossin/grocerytracker/screens/Login.java
+++ app/src/main/java/com/stevecrossin/grocerytracker/screens/Login.java
@@ -3,7 +3,7 @@ package com.stevecrossin.grocerytracker.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
-    /* Login code for the application will go here.
+    /* Login code for the application will go here..
 
       Some of the functions that will need to be performed are:
       1. Initialise the UI elements on the screen, setup current login state
